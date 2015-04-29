f2192e325ffa37876fed7ef127f87962d17523fe
Lucas Yan <lucasyan@berkeley.edu>
2015-04-29 04:03:29 -0700

    Layout changes, code cleanup, removal of unneeded navigation and flip elements. Replaced with a fixed cover, pretty typing animations, and a hover to replace flip. Also fixed all the dimensions to make it perfectly responsive

--- js/script.js
+++ js/script.js
@@ -1,2 +1,33 @@
-//Typer.js call method for landing section
+// TyperJS initialization.
 $('[data-typer-targets]').typer();
+
+var previousScroll = 0;
+var threshold = 110;
+var duration = 300;
+
+// Resizable navigation bar.
+$(window).scroll(function(){
+    
+    if(previousScroll < threshold && $(window).scrollTop() > threshold) {
+        $('.section-launch').animate({
+            height: "140px"
+        }, {duration: duration, queue: false});
+        
+        $('.portrait').animate({
+            height: "80px",
+            width: "80px"
+        }, {duration: duration, queue: false});
+        previousScroll = $(window).scrollTop();
+    }
+    else if(previousScroll > threshold && $(window).scrollTop() < threshold) {
+        $('.section-launch').animate({
+            height: "350px"
+        }, {duration: duration, queue: false});
+        
+        $('.portrait').animate({
+            height: "125px",
+            width: "125px"
+        }, {duration: duration, queue: false});
+        previousScroll = $(window).scrollTop();
+    }
+});
